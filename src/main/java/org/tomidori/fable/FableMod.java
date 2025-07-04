a486bdb44a723aee4ec9e583d3e04e85b9bc3adf
Tomidori <219072054+tomidori@users.noreply.github.com>
2025-07-04 20:17:43 +0900

    Initial commit

---
+++ src/main/java/org/tomidori/fable/FableMod.java
@@ -0,0 +1,9 @@
+package org.tomidori.fable;
+
+import net.fabricmc.api.ModInitializer;
+
+public final class FableMod implements ModInitializer {
+    @Override
+    public void onInitialize() {
+    }
+}
