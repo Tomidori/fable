ce71118d025e2d0afe7dd350f0e7f77dbb235ddf
Tomidori <219072054+tomidori@users.noreply.github.com>
2025-07-04 20:57:53 +0900

    Ensure skills are terminated when players die

---
+++ src/main/java/org/tomidori/fable/mixin/ServerPlayerEntityMixin.java
@@ -0,0 +1,17 @@
+package org.tomidori.fable.mixin;
+
+import net.minecraft.entity.damage.DamageSource;
+import net.minecraft.server.network.ServerPlayerEntity;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
+import org.tomidori.fable.entity.LivingEntityHook;
+
+@Mixin(value = ServerPlayerEntity.class)
+public abstract class ServerPlayerEntityMixin {
+    @Inject(method = "onDeath", at = @At("TAIL"))
+    private void fable$onDeath(DamageSource source, CallbackInfo ci) {
+        ((LivingEntityHook) this).getSkillManager().terminateCastingSkill();
+    }
+}
