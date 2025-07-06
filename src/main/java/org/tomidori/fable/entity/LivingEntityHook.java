df4c3d87fb94be39a12be9cb51e89195e4899793
Tomidori <219072054+tomidori@users.noreply.github.com>
2025-07-06 18:29:34 +0900

    Add default implementation for LivingEntityHook.getSkillManager

--- src/main/java/org/tomidori/fable/entity/LivingEntityHook.java
+++ src/main/java/org/tomidori/fable/entity/LivingEntityHook.java
@@ -3,5 +3,7 @@ package org.tomidori.fable.entity;
 import org.tomidori.fable.skill.manager.SkillManager;
 
 public interface LivingEntityHook {
-    SkillManager getSkillManager();
+    default SkillManager getSkillManager() {
+        throw new UnsupportedOperationException("Implemented via mixin");
+    }
 }
